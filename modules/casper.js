8ed02aa2b379fac0c8a761cc94ee1334d0cadecc
Mathias Malmqvist <mathias.malmqvist@gmail.com>
2016-12-08 15:48:08 +0100

    Added dumpSteps() from https://github.com/yotsumoto/casperjs-goto

--- modules/casper.js
+++ modules/casper.js
@@ -2793,3 +2793,53 @@ function createPage(casper) {
     casper.emit('page.created', page);
     return page;
 }
+
+
+
+
+// Added dumpStep() from https://github.com/yotsumoto/casperjs-goto /Mathias
+
+/**
+ * Dump Navigation Steps for debugging
+ * When you call this function, you cat get current all information about CasperJS Navigation Steps
+ * This is compatible with label() and goto() functions already.
+ *
+ * @param   Boolen   showSource    showing the source code in the navigation step?
+ *
+ * All step No. display is (steps array index + 1),  in order to accord with logging [info] messages.
+ *
+ */
+Casper.prototype.dumpSteps = function dumpSteps(showSource) {
+  this.echo( "=========================== Dump Navigation Steps ==============================", "RED_BAR");
+  if(this.current) { 
+    this.echo( "Current step No. = " + (this.current+1) , "INFO"); 
+  }
+  this.echo( "Next    step No. = " + (this.step+1) , "INFO");
+  this.echo( "steps.length = " + this.steps.length , "INFO");
+  this.echo( "================================================================================", "WARNING" );
+
+  for(var i=0; i<this.steps.length; i++) {
+    var step  = this.steps[i];
+    var msg   = "Step: " + (i+1) + "/" + this.steps.length + "     level: " + step.level
+    if( step.executed ) { 
+        msg = msg + "     executed: " + step.executed 
+    }
+    var color = "PARAMETER";
+    if(step.label) { 
+        color = "INFO"; msg = msg + "     label: " + step.label 
+    }
+
+    if(i == this.current) {
+      this.echo( msg + "     <====== Current Navigation Step.", "COMMENT");
+    } else {
+      this.echo( msg, color );
+    }
+    if(showSource) {
+      this.echo( "--------------------------------------------------------------------------------" );
+      this.echo( this.steps[i] );
+      this.echo( "================================================================================", "WARNING" );
+    }
+  }
+};
+
+
